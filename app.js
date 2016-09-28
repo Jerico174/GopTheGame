0e631a2c4f3ca052591a4fe0830e5a0966135020
lavrov <lavrovv-v@yandex.ru>
2016-09-28 23:02:09 +0500

    + Добавлено подключение к базе «gop».

--- app.js
+++ app.js
@@ -33,6 +33,23 @@ app.use(function(req, res, next){
 	next(err);
 });
 
+var mysql = require('mysql'),
+	connection = mysql.createConnection({
+		host: 'localhost',
+		database: 'gop',
+		user: 'root',
+		password: ''
+	});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+	if (err) throw err;
+	console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
 // error handlers
 
 // development error handler
